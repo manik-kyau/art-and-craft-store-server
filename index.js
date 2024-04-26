79baa55c0029614c24922278f76a339da56058db
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-04-26 21:50:04 +0600

    Complate create and read data functionality

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jm3t3oc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +24,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    // const database = client.db("sample_mflix");
+    const craftCollection = client.db("craftsDB").collection("crafts");
+
+    // get craft data
+    app.get('/crafts',async(req,res)=>{
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // post craft
+    app.post('/crafts',async(req,res)=>{
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await craftCollection.insertOne(newCraft);
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
